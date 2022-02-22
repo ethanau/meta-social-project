6dbc2c97e3d7bde0675402545710097e64703cb0
yebinge <yebing@student.unimelb.edu.au>
2022-02-22 14:44:55 +0800

    Complete accounts app

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth import models
+from django.utils import timezone
+
 
 # Create your models here.
+class User(models.User, models.PermissionsMixin):
+
+    def __str__(self):
+        return "@{}".format(self.username)
